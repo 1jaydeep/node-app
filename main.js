7eda9ca3a2b13bfe29c9bb315b76668228d12bef
1jaydeep <jaydeep.elluminati@gmail.com>
2025-04-15 12:47:54 +0530

    port update

--- main.js
+++ main.js
@@ -18,5 +18,5 @@ const server = http.createServer((req, res) => {
 });
 
 server.listen(PORT, () => {
-    console.log(`🚀 Server running at http://localhost:${PORT}`);
+    console.log(` Server running at http://localhost:${PORT}`);
 });
